806298e709909293c7c7d7b84ba980c57969829f
adam allalou <aabou9@gmail.com>
2020-09-15 11:24:49 +0100

    add refactored files from index

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import GameScene from "./GameScene";
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: GameScene
+};
+export default config
--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser, { Game } from "phaser";
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
+import config from "./config";
 
-const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
-}
+const game = new Game(config);
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
