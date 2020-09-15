e398c79b03bef7075d4fcbd982633a75d4385dd8
adam allalou <aabou9@gmail.com>
2020-09-15 17:10:28 +0100

    fix sprite image

--- src/GameScene.js
+++ src/GameScene.js
@@ -15,7 +15,7 @@ class GameScene extends Scene {
     this.load.image('ground', imgplatform);
     this.load.image('star', imgstar);
     this.load.image('bomb', imgvirus);
-    this.load.spritesheet('dude', imgdude, { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('dude', imgdude, { frameWidth: 114, frameHeight: 115 });
   }
 
 
@@ -28,7 +28,32 @@ class GameScene extends Scene {
     player = this.physics.add.sprite(100, 450, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
+
     
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 8 }),
+      frameRate: 15,
+      repeat: -1
+  });
+
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 9 } ],
+      frameRate: 20
+  });
+
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 19, end: 10 }),
+      frameRate: 15,
+      repeat: -1
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+
     function createPlatform() {
       platforms.create(400, 568, 'ground').setScale(2).refreshBody();
       platforms.create(600, 400, 'ground');
@@ -36,5 +61,35 @@ class GameScene extends Scene {
       platforms.create(750, 220, 'ground');
     };
   }
+
+  
+  update ()
+  {
+      if (cursors.left.isDown)
+      {
+          player.setVelocityX(-180);
+
+          player.anims.play('left', true);
+      }
+      else if (cursors.right.isDown)
+      {
+          player.setVelocityX(180);
+
+          player.anims.play('right', true);
+      }
+      else
+      {
+          player.setVelocityX(0);
+
+          player.anims.play('turn');
+      }
+
+      if (cursors.up.isDown && player.body.touching.down)
+      {
+          player.setVelocityY(-330);
+      }
+  }
 }
+
+
 export default GameScene;
